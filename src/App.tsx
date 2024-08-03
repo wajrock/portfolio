f727339386eb5da824b3aa4e686ec448cf608ad1
twajrock <thibaud.wajrock@etudiant.univ-rennes1.fr>
2024-08-03 16:29:20 +0200

    Routage

--- src/App.tsx
+++ src/App.tsx
@@ -30,14 +30,14 @@ function App() {
   
   
   return (
-    <Router>
+    <Router basename="/portfolio">
         <ScrollToTop />
         <Routes>
-          <Route path="/portfolio/" element={<Navigate to="/portfolio/en/" replace />} />
-          <Route path="/portfolio/en/" element={<HomeEN />} />
-          <Route path="/portfolio/fr/" element={<HomeFR />} />
-          <Route path="/portfolio/en/project/:projectId" element={<WorkEN />} />
-          <Route path="/portfolio/fr/projet/:projectId" element={<WorkFR />} />
+          <Route path="/" element={<Navigate to="/en/" replace />} />
+          <Route path="/en/" element={<HomeEN />} />
+          <Route path="/fr/" element={<HomeFR />} />
+          <Route path="/en/project/:projectId" element={<WorkEN />} />
+          <Route path="/fr/projet/:projectId" element={<WorkFR />} />
         </Routes>
     </Router>
   );
