becd92b10da85b253223e2d27757691422dbf068
wajrock <thibaud.wajrock@outlook.com>
2026-05-28 19:59:25 +0200

    update references layout section

--- src/app/features/home-page/references-section/references-section.component.ts
+++ src/app/features/home-page/references-section/references-section.component.ts
@@ -2,10 +2,6 @@ import {Component, ElementRef, signal, viewChild} from '@angular/core';
 import {SCROLL_DIRECTION, CAROUSEL_SECTION} from '../../../shared/models';
 import {ButtonComponent} from '../../../shared/components/button/button.component';
 
-enum REFERENCE_VIEW {
-  HARMONIC = 'harmonic',
-  LEROYMERLIN = 'leroymerlin',
-}
 @Component({
   selector: 'app-references-section',
   imports: [ButtonComponent],
@@ -16,13 +12,10 @@ export class ReferencesSectionComponent {
   // CONSTANTS
   readonly SCROLL_DIRECTION = SCROLL_DIRECTION;
   readonly CAROUSEL_SECTION = CAROUSEL_SECTION;
-  readonly REFERENCE_VIEW = REFERENCE_VIEW;
 
   // SIGNALS
   disablePreviousButton = signal<boolean>(true);
   disabledNextButton = signal<boolean>(false);
-
-  activeReference = signal<REFERENCE_VIEW>(REFERENCE_VIEW.HARMONIC);
 
   // MISC
   referencesCarousel = viewChild<ElementRef<HTMLDivElement>>('referencesCarousel');
